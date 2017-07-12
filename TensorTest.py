58b1f16edf30f4fc1d05ecd0b53648b521a6489b
Tim <tgerstel@umass.edu>
2017-07-12 17:16:44 -0400

    Matrix Multiplication and Configuration

--- TensorTest.py
+++ TensorTest.py
@@ -1,4 +1,14 @@
 import tensorflow as tf
-hello = tf.constant('Hello Tensor')
-sess = tf.Session()
-print(sess.run(hello))
+#Test Machine does not currently have >= Compute Compatibility 3.0
+cfg = tf.ConfigProto()
+cfg.log_device_placement = False
+cfg.gpu_options.allow_growth = True
+with tf.device('/cpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=cfg)
+# Runs the op.
+print(sess.run(c))
+print(tf.nn.sigmoid(1.2, 'c'))
